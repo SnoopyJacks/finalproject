f794743b3a02752470f4c51150d5f3a9bcc853c5
Justine <t.nichole.2807@gmail.com>
2026-06-20 08:44:21 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+      <h1>Weather App</h1>
+      <footer>
+      This project was coded by Justine Ziola and is open-sourced on <a href="https://github.com/justineziola/weather-app" target="_blank" rel="noopener noreferrer">GitHub</a> and is deployed on <a href="https://finalprojectweatherap.netlify.app" target="_blank" rel="noopener noreferrer">Netlify</a>.
+      </footer>
+    </div>
+    </div>
+  );
 }
 
